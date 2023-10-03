a5cbfae76c0197e102a1ab8a3f69a1f6ac921159
yeeuniii <anna862700@gmail.com>
2023-10-03 22:13:35 +0900

    Feat: Location 클래스 한 줄씩 directive 구분

--- Location.cpp
+++ Location.cpp
@@ -1,10 +1,86 @@
 
 #include "Location.hpp"
 
+#include <iostream>
+#include <cctype>
 
-Location::Location(const std::string &location_block) {
+Location::Location() {}
 
-    /**
-     * 1. location 객체 값 넣기
-     */
+Location::Location(std::istringstream& location_block) : root("")
+{
+	std::string line;
+
+	while (location_block.eof() == false)
+	{
+		std::getline(location_block, line, ';');
+		parse(line);
+	}
+}
+
+Location::Location(const Location &other)
+{
+	*this = other;
+}
+
+Location &Location::operator=(const Location &other)
+{
+	if (this != &other)
+	{
+		this->http_methods = other.http_methods;
+		this->return_value = other.return_value;
+		this->root = other.root;
+		this->index = other.index;
+		this->autoindex = other.autoindex;
+	}
+	return *this;
+}
+
+Location::~Location() {}
+
+void Location::parse(std::string &line) {
+	std::stringstream ss(line);
+	std::string	directive;
+	
+	ss >> directive;
+	if (directive == "allow_methods") {
+		parseMethod();
+		return ;
+	}
+	if (directive == "return") {
+		parseReturnValue();
+		return ;
+	}
+	if (directive == "root") {
+		parseRoot();
+		return ;
+	}
+	if (directive == "index") {
+		parseIndex();
+		return ;
+	}
+	if (directive == "autoindex") {
+		parseAutoindex();
+		return ;
+	}
+	throw (std::invalid_argument("Error: Invalid directive '" + directive + "'"));
+}
+
+void Location::parseMethod() {
+
+}
+
+void Location::parseReturnValue() {
+
+}
+
+void Location::parseRoot() {
+
+}
+
+void Location::parseIndex() {
+
+}
+
+void Location::parseAutoindex() {
+
 }
--- Location.hpp
+++ Location.hpp
@@ -1,27 +1,36 @@
 
-#ifndef WEBSERV_LOCATION_HPP
-#define WEBSERV_LOCATION_HPP
+#ifndef LOCATION_HPP
+#define LOCATION_HPP
 
 #include <string>
+#include <sstream>
 #include <set>
 #include <vector>
+#include <stdexcept>
 
 class Location {
 
 protected:
     std::set<std::string> http_methods;
-    std::set<std::string> http_redirection;
+    std::pair<int, std::string> return_value;
     std::string root;
     std::vector<std::string> index;
     bool autoindex;
+	
+	void parse(std::string& line);	
+	void parseMethod();
+	void parseReturnValue();
+	void parseRoot();
+	void parseIndex();
+	void parseAutoindex();
 
 public:
-
     Location();
-
-    Location(const std::string &location_block);
-
+    Location(std::istringstream& location_block);
+    Location(const Location& other);
+	Location& operator=(const Location& other);
+    ~Location();
 };
 
 
-#endif //WEBSERV_LOCATION_HPP
+#endif //LOCATION_HPP

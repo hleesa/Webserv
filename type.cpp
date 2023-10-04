d51e79e7c3ce628ddf12d6ad8b4e4ec928cf2a6e
yeeuniii <anna862700@gmail.com>
2023-10-04 16:36:51 +0900

    Feat: integer 형식의 string인지 확인하는 함수 구현

--- type.cpp
+++ type.cpp
@@ -1,13 +1,23 @@
-#include <string>
+#include "type.hpp"
 
-bool isHttpMethod(const std::string& method)
+bool isHttpMethod(const std::string& string)
 {
 	std::string allow_methods[9] = {"GET", "POST", "PUT", \
 								"PATCH", "DELETE", "HEAD", \
 								"OPTIONS", "CONNECT", "TRACE"};
 	int idx = 0;
 
-	while (idx < 9 && method != allow_methods[idx])
+	while (idx < 9 && string != allow_methods[idx])
 		idx++;
 	return idx != 9;
+}
+
+bool	isInteger(const std::string& string)
+{
+	int	idx = 0;
+	int	size = string.size();
+
+	while (idx < size && isdigit(string[idx]))
+		idx++;
+	return idx == size;
 }
---
+++ type.hpp
@@ -0,0 +1,10 @@
+
+#ifndef TYPE_HPP
+#define TYPE_HPP
+
+#include <string>
+
+bool isHttpMethod(const std::string& string);
+bool isInteger(const std::string& string);
+
+#endif

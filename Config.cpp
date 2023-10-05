adca875d031c89f1c059d7cc27e79e1d19b98d3a
salee2 <hleesa>
2023-10-05 22:11:03 +0900

    refactor: Config class

--- Config.cpp
+++ Config.cpp
@@ -12,25 +12,30 @@
 * 3. server {}의 내용물만
 **/
 
-
-void print(std::istringstream& ss) {
-    std::string line;
-    while (std::getline(ss, line)) {
-        std::cout << line << '\n';
+std::string semicolonTrim(const std::string& line) {
+    std::string ret = line;
+    const std::string space = " \t\n\v\f\r";
+    if (!ret.empty() && ret.back() == ';') {
+        ret.pop_back();
+        size_t end = ret.find_last_not_of(space);
+        ret = ret.substr(0, end + 1);
+        ret.append(";");
     }
+    return ret;
 }
 
 std::string trim(const std::string& input) {
-    size_t start = input.find_first_not_of(" \t\n\v\f\r");
+    const std::string space = " \t\n\v\f\r";
+    size_t start = input.find_first_not_of(space);
     if (start == std::string::npos) {
         return "";
     }
-    size_t end = input.find_last_not_of(" \t\n\v\f\r");
-    return input.substr(start, end - start + 1);
+    size_t end = input.find_last_not_of(space);
+    return semicolonTrim(input.substr(start, end - start + 1));
 }
 
 bool isValidLineTerminator(const std::string& line) {
-    const std::string line_term = ";{}";
+    const std::string line_term = "{;}";
     if (line.empty()) {
         return true;
     }
@@ -47,20 +52,6 @@ bool isValidLineTerminator(const std::string& line) {
         num_of_line_term += counter[*it];
     }
     return num_of_line_term == 1;
-}
-
-std::string semicolonTrim(const std::string& line) {
-    std::string ret = line;
-    if (!ret.empty() && ret.back() == ';') {
-        ret.pop_back();
-        std::string::reverse_iterator it = ret.rbegin();
-        while (it != line.rend() && isspace(*it)) {
-            ret.pop_back();
-            ++it;
-        }
-        ret.append(";");
-    }
-    return ret;
 }
 
 bool isValidServerOpen(const std::string& line) {
@@ -89,40 +80,94 @@ bool isValidLocationOpen(const std::string& line) {
     return (i + 1) == line.length();
 }
 
-bool isValidConfigLine(std::string& line, std::stack<char>& brace_stack) {
+enum lineType getLineType(std::string& line, std::stack<char>& brace_stack) {
     if (!isValidLineTerminator(line)) {
-        return false;
+        return INVALID;
     }
-    line = semicolonTrim(line);
-    switch (line.back()) {
-        case '{':
-            if (brace_stack.empty()) {
-                if (!isValidServerOpen(line)) {
-                    return false;
-                }
-            }
-            else if (!isValidLocationOpen(line)) {
-                return false;
-            }
-            brace_stack.push(line.back());
-            break;
-        case '}':
-            if (brace_stack.empty() || brace_stack.top() != '{') {
-                return false;
-            }
-            else if (line.length() != 1) {
-                return false;
-            }
-            brace_stack.pop();
-            break;
+    else if (line.empty()) {
+        return SPACE;
     }
-    return true;
+    else if (line.front() == '#') {
+        return COMMENT;
+    }
+    else if (line.back() == '{') {
+        if (brace_stack.empty() && isValidServerOpen(line)) {
+            return SERVER_OPEN;
+        }
+        else if (!brace_stack.empty() && isValidLocationOpen(line) ) {
+            return LOCATION_OPEN;
+        }
+    }
+    else if (line.back() == ';') {
+        return SEMICOLON;
+    }
+    else if (line.back() == '}') {
+        if (brace_stack.empty() || brace_stack.top() != '{' || line.length() != 1) {
+            return INVALID;
+        }
+        else if (brace_stack.size() == 2) {
+            return LOCATION_CLOSE;
+        }
+        else if (brace_stack.size() == 1) {
+            return SERVER_CLOSE;
+        }
+    }
+    return INVALID;
+}
+
+std::vector<std::string> getSeverContents(std::ifstream& config) {
+    const int content_size = 500;
+    std::string line;
+    std::string server_content;
+    std::vector<std::string> server_contents;
+    std::stack<char> brace_stack;
+    while (std::getline(config, line)) {
+        line = trim(line);
+        enum lineType line_type = getLineType(line, brace_stack);
+        switch (line_type) {
+            case SPACE:
+            case COMMENT:
+                continue;
+                break;
+            case INVALID:
+                throw std::invalid_argument("Error: Syntax");
+                break;
+            case SERVER_OPEN:
+                brace_stack.push(line.back());
+                server_content.clear();
+                server_content.reserve(content_size);
+                break;
+            case LOCATION_OPEN:
+                brace_stack.push(line.back());
+                server_content.append(line + "\n");
+                break;
+            case SEMICOLON:
+                server_content.append(line + "\n");
+                break;
+            case LOCATION_CLOSE:
+                brace_stack.pop();
+                server_content.append(line + "\n");
+                break;
+            case SERVER_CLOSE:
+                brace_stack.pop();
+                server_contents.push_back(server_content);
+                break;
+        }
+    }
+    return server_contents;
+}
+
+
+void print(std::istringstream& ss) {
+    std::string line;
+    while (std::getline(ss, line)) {
+        std::cout << line << '\n';
+    }
 }
 
 Config::Config(const std::string& config_file) {
 
     const std::string config_format = ".conf";
-
     if (config_file.length() < config_format.length()) {
         throw std::invalid_argument("Error: Invalid file name '" + config_file + "'");
     }
@@ -133,46 +178,11 @@ Config::Config(const std::string& config_file) {
     if (!config.is_open()) {
         throw std::invalid_argument("Error: Failed to open file '" + config_file + "'");
     }
-    std::vector<std::string> server_contents;
-    std::string line;
-    std::stack<char> brace_stack;
-    while (std::getline(config, line)) {
-        line = trim(line); // server out
-        if (line.front() == '#' || line.empty()) {
-            continue;
-        }
-        else if (isValidConfigLine(line, brace_stack)) {
-            if (brace_stack.size() == 1) { // server in
-                std::string server_content;
-                server_content.reserve(500);
-                while (std::getline(config, line)) {
-                    line = trim(line);
-                    if (line.front() == '#') {
-                        continue;
-                    }
-                    else if (isValidConfigLine(line, brace_stack)) {
-                        if (brace_stack.empty()) break;
-                        server_content.append(line);
-                        server_content.append("\n");
-                    }
-                    else {
-                        throw std::invalid_argument("Error: Syntax");
-                    }
-                }
-                server_contents.push_back(server_content);
-            }
-            else {
-                throw std::invalid_argument("Error: Syntax");
-            }
-        }
-        else {
-            throw std::invalid_argument("Error: Syntax");
-        }
-    }
+    std::vector<std::string> server_contents = getSeverContents(config);
+
     /**
      * test
      */
-
     for (std::vector<std::string>::iterator it = server_contents.begin(); it != server_contents.end(); ++it) {
         std::istringstream ifs(*it);
         print(ifs);
